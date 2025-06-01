1c1dc6f6e8a6548f364ca4e5c6107476d72e6f8b
Abdallh Mostafa elRabiey <abdallhelrabiey255@gmail.com>
2025-06-01 16:23:11 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 
 import 'package:fanzhashem/modules/auth/login/login.dart';
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 
 
-import 'models/user_model.dart';
-
-import 'modules/page_view/page_view.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 // void main() {
@@ -16,13 +12,7 @@ import 'package:easy_localization/easy_localization.dart';
 // }
 
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-
-import 'modules/user_profile/cubit/cubit.dart';
-import 'modules/user_profile/cubit/states.dart';
-import 'translations/codegen_loader.g.dart';
 
 late SharedPreferences preferences;
 
